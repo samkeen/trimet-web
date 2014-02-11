3b220f43e87a457361dc4be0b21171ec5e408484
samkeen <sam.keen@janrain.com>
2014-02-10 22:35:22 -0800

    added simple erb template

--- app.rb
+++ app.rb
@@ -17,13 +17,6 @@ get '/stop/:location_id' do
   location_id = params[:location_id]
   stop = trimet.stop(location_id)
 
-  location = stop.location
-
-  output = "<pre>For Transit stop: #{location['locid']} : #{location['desc']} (#{location['dir']})\n"
-
-  stop.arrivals.each do |bus|
-    output += "\t#{bus['shortSign']} will arrive at #{bus['estimated']}\n"
-  end
-  output
+  erb :stop, :locals => {location: stop.location, arrivals: stop.arrivals}
 end
 
